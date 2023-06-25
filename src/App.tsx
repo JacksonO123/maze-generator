0ab99629cf08589b66405cce499056d34cf5a2af
Jackson Otto <jacksonotto120@gmail.com>
2023-06-25 02:27:12 -0700

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,41 @@ import {
   Vector,
 } from "simulationjs";
 import "./App.css";
-import { generate, lonelyWall } from "./utils/split-gen";
+import { generate, hasDiagonalConnection, path, wall } from "./utils/split-gen";
+
+export type Grid = number[][];
+
+export class Coord {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  stringify() {
+    return `${this.x}:${this.y}`;
+  }
+
+  static fromString(str: string) {
+    const parts = str.split(":");
+
+    const x = +parts[0];
+    const y = +parts[1];
+
+    return new Coord(x, y);
+  }
+}
 
 const App = () => {
   let canvasRef: HTMLCanvasElement;
   let canvas: Simulation;
   let mazeSquares: SceneCollection;
-  let maze: number[][];
+  let maze: Grid;
 
   const size = 500;
-  const gridSize = 21;
+  const gridSize = 41;
 
   setTimeout(() => {
     canvas = new Simulation(canvasRef);
@@ -31,14 +56,22 @@ const App = () => {
 
     canvas.on("click", (e: MouseEvent) => {
       const p = new Vector(e.offsetX, e.offsetY);
+
       const x = Math.floor(p.x / (size / gridSize));
       const y = Math.floor(p.y / (size / gridSize));
 
-      console.log(lonelyWall(maze, x, y));
+      console.log({
+        topLeft: hasDiagonalConnection(maze, x, y, -1, -1),
+        topRight: hasDiagonalConnection(maze, x, y, 1, -1),
+        bottomLeft: hasDiagonalConnection(maze, x, y, -1, 1),
+        bottomRight: hasDiagonalConnection(maze, x, y, 1, 1),
+      });
+
+      console.log(x, y);
     });
   });
 
-  function drawMaze(maze: number[][]) {
+  function drawMaze(maze: Grid) {
     const squareSize = size / gridSize;
 
     mazeSquares.empty();
@@ -46,15 +79,17 @@ const App = () => {
       row.forEach((item, j) => {
         const x = j * squareSize + squareSize / 2;
         const y = i * squareSize + squareSize / 2;
-        if (item === 0) {
-          const square = new Square(
-            new Vector(x, y),
-            squareSize,
-            squareSize,
-            new Color(0, 0, 0)
-          );
-          mazeSquares.add(square);
-        }
+        const square = new Square(
+          new Vector(x, y),
+          squareSize,
+          squareSize,
+          item === wall
+            ? new Color(0, 0, 0)
+            : item === path
+            ? new Color(255, 255, 255)
+            : new Color(0, 0, 255)
+        );
+        mazeSquares.add(square);
       });
     });
   }
--- src/utils/split-gen.ts
+++ src/utils/split-gen.ts
@@ -1,8 +1,10 @@
-const wall = 0;
-const path = 1;
+import { Coord, Grid } from "../App";
+
+export const wall = 0 as const;
+export const path = 1 as const;
 
 function initGrid(size: number) {
-  const res: number[][] = [];
+  const res: Grid = [];
 
   for (let i = 0; i < size; i++) {
     const temp: number[] = [];
@@ -16,12 +18,12 @@ function initGrid(size: number) {
 }
 
 function splitIteration(
-  grid: number[][],
+  grid: Grid,
   xStart: number,
   yStart: number,
   xBound: number,
   yBound: number,
-  history: number[][][]
+  history: Grid[]
 ) {
   if (xBound - xStart < 3) return grid;
   if (yBound - yStart < 3) return grid;
@@ -29,8 +31,8 @@ function splitIteration(
   const randomRow = Math.min(randomRangeEven(yBound, yStart) + 1, yBound - 2);
   const randomCol = Math.min(randomRangeEven(xBound, xStart) + 1, xBound - 2);
 
-  const randomSplitCol = Math.min(randomRange(yBound, yStart) + 1, yBound - 2);
-  const randomSplitRow = Math.min(randomRange(yBound, yStart) + 1, xBound - 2);
+  let randomSplitCol = Math.min(randomRange(yBound, yStart) + 1, yBound - 2);
+  let randomSplitRow = Math.min(randomRange(xBound, xStart) + 1, xBound - 2);
 
   for (let i = yStart; i < yBound; i++) {
     if (i === 0 || i === grid.length - 1) continue;
@@ -57,7 +59,7 @@ function splitIteration(
   return grid;
 }
 
-export function lonelyWall(grid: number[][], x: number, y: number) {
+function lonelyWall(grid: Grid, x: number, y: number) {
   if (x - 1 < 0 || x + 1 > grid.length || y - 1 < 0 || y + 1 > grid.length)
     return false;
 
@@ -69,15 +71,19 @@ export function lonelyWall(grid: number[][], x: number, y: number) {
   );
 }
 
-function fixMaze(grid: number[][]) {
-  const toAdd: [number, number][] = [];
-
+function fixWalls(grid: Grid) {
   for (let i = 0; i < grid.length; i++) {
     grid[i][0] = wall;
     grid[i][grid.length - 1] = wall;
     grid[0][i] = wall;
     grid[grid.length - 1][i] = wall;
   }
+
+  return grid;
+}
+
+function fixLonelyWalls(grid: Grid) {
+  const toAdd: Coord[] = [];
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid.length; j++) {
@@ -102,29 +108,116 @@ function fixMaze(grid: number[][]) {
   }
 
   toAdd.forEach((item) => {
-    grid[item[0]][item[1]] = wall;
+    grid[item.y][item.x] = wall;
   });
 
   return grid;
 }
 
-export function generate(size: number, drawCb: (grid: number[][]) => void) {
+export function hasDiagonalConnection(
+  grid: Grid,
+  x: number,
+  y: number,
+  xOffset: number,
+  yOffset: number
+) {
+  return (
+    grid[y]?.[x] === wall &&
+    grid[y + yOffset]?.[x + xOffset] === wall &&
+    grid[y]?.[x + xOffset] === path &&
+    grid[y + yOffset]?.[x] === path
+  );
+}
+
+function fixDiagonalConnections(grid: Grid, cuttoff = 0) {
+  const offsetCounts: Coord[] = [
+    new Coord(-1, -1),
+    new Coord(1, -1),
+    new Coord(-1, 1),
+    new Coord(1, 1),
+  ];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid.length; j++) {
+      if (grid[i][j] === path) continue;
+
+      const toAdd: Coord[] = [];
+      const posCount = new Map<string, number>();
+
+      const updateCounts = (offset: string) => {
+        const oldVal = posCount.get(offset);
+
+        if (oldVal === undefined) {
+          posCount.set(offset, 1);
+        } else {
+          posCount.set(offset, oldVal + 1);
+        }
+      };
+
+      for (const offset of offsetCounts) {
+        const res = hasDiagonalConnection(grid, j, i, offset.x, offset.y);
+        if (res) {
+          const pos1 = new Coord(j + offset.x, i).stringify();
+          updateCounts(pos1);
+
+          const pos2 = new Coord(j, i + offset.y).stringify();
+          updateCounts(pos2);
+        }
+      }
+
+      const entries = [...posCount.entries()];
+      if (entries.length > 0) {
+        const filtered = entries.filter((item, _) => item[1] > cuttoff);
+        if (filtered.length > 0) {
+          toAdd.push(Coord.fromString(filtered[0][0]));
+        }
+      }
+
+      toAdd.forEach((pos) => {
+        grid[pos.y][pos.x] = wall;
+      });
+    }
+  }
+
+  return grid;
+}
+
+function fixMaze(grid: Grid) {
+  grid = fixWalls(grid);
+  grid = fixLonelyWalls(grid);
+  grid = fixDiagonalConnections(grid, 1);
+  grid = fixDiagonalConnections(grid);
+
+  return grid;
+}
+
+export function generate(size: number, drawCb: (grid: Grid) => void) {
   let grid = initGrid(size);
 
-  const history: number[][][] = [];
+  const history: Grid[] = [];
   grid = splitIteration(grid, 0, 0, grid.length, grid.length, history);
 
   grid = fixMaze(grid);
   history.push([...grid.map((row) => [...row])]);
 
   // // for animating
-  // const delay = 0;
+  // const delay = 500;
 
   // history.forEach((grid, i) => {
   //   setTimeout(() => {
   //     drawCb(grid);
   //   }, delay * i);
-  // });
+  //j});
+
+  let index = history.length - 1;
+  addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+      index = Math.max(0, index - 1);
+    } else if (e.key === "ArrowRight") {
+      index = Math.min(history.length - 1, index + 1);
+    }
+    drawCb(history[index]);
+  });
 
   return grid;
 }
